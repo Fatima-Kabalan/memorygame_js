ed328c7e0a354664fa1da47a0040e912eba19d96
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-09-16 20:04:12 +0300

    changing the layout of cards and color and trying to match cards using javascript

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,30 @@
 const cards = document.querySelectorAll('.card');
+// const btn = document.getElementById("btn");
 
-cards.forEach((card)=>{
-  card.addEventListener( 'click', function() {
-    card.classList.toggle('flip');
-  });
-});
+// cards.forEach((card)=>{
+//   card.addEventListener( 'click', function() {
+//     card.classList.toggle('flip');
+//   });
+// });
+
+// console.log(cards);
+
+let flippedcard = false;
+let firstcard , secondcard ;
+
+function  flipcard() {
+    this.classList.add('flip');
+    if (flippedcard){
+        //first click for the player
+        flippedcard = true ;
+        firstcard = this ;
+        console.log({flippedcard});
+    }
+    else{
+        flippedcard = false;
+        secondcard = this ;
+    }
+}
+
+
+cards.forEach(card => card.addEventListener('click',flipcard))
