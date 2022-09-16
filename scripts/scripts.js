06bf6d5fabdbcdae503fd141fdbb91dbbce47ee5
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2022-09-16 22:27:30 +0300

    adding a score and an alert that tells if the player wins or loses

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,37 +1,68 @@
 const cards = document.querySelectorAll('.card');
-// const btn = document.getElementById("btn");
-
+const p = document.getElementById("parag");
 // cards.forEach((card)=>{
 //   card.addEventListener( 'click', function() {
 //     card.classList.toggle('flip');
 //   });
 // });
 
-// console.log(cards);
-
 let flippedcard = false;
-let firstcard , secondcard ;
+let firstcard, secondcard ;
+let score = 0 ;
+let count = 0 ;
 
 function  flipcard() {
     this.classList.add('flip');
-    if (flippedcard){
+    if (!flippedcard){
         //first click for the player
         flippedcard = true ;
-        firstcard = this ;
+        firstcard = this;
         console.log({flippedcard});
+        count++;
+        console.log(count);
+
     }
     else{
         flippedcard = false;
         secondcard = this ;
-
-        // Check matching cards 
-        // console.log(firstcard.name);
-        console.log(secondcard.dataset.name);
-        
+        count++;
+        console.log(count);
+        checkMatch();
     }
-
-
+}
+  
+function checkMatch(){
+    // Check matching cards 
+    let a = firstcard.dataset.name;
+    let b = secondcard.dataset.name;
+//    let score = 0 ;
+    if(a == b){
+        score++;
+        firstcard.removeEventListener('click',flipcard);
+        secondcard.removeEventListener('click',flipcard);
+        p.innerHTML = score;            
+    }else{
+        //cards are not matching 
+        // settimeout is to have enough time to see cards flipping 
+        setTimeout( () => {
+            disablecards();
+        } , 100 );
+    }
+    if (count == 6 ){
+        if(score == 3){
+            alert("You WIN!!!")
+        }else{
+            alert("YOU LOSE!")
+        }
+    }
 }
 
 
-cards.forEach(card => card.addEventListener('click',flipcard))
+function disablecards(){
+    firstcard.removeEventListener('click',flipcard);
+    secondcard.removeEventListener('click',flipcard);
+}
+
+
+
+cards.forEach(card => card.addEventListener('click',flipcard));
